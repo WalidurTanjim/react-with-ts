642aaad2040117a8fd20c2afcc0ebfa4d46e06fa
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-07-15 01:51:19 +0600

    Added useState, useEffect, Increment count, set new user on user state

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 import Greeting from './components/Greeting/Greeting'
 
+interface UserInterface {
+  name: string;
+  email: string;
+}
+
 function App() {
+  const [count, setCount] = useState<number>(0);
+  const [user, setUser] = useState<UserInterface | null>(null);
+
   const name = "Walidur Tanjim";
   const age = 25;
+
+  useEffect((): void => {
+    setUser({name: "WALIDUR TANJIM", email: "walidur.tanjim@gmail.com"})
+  }, []);
 
   return (
     <>
       <h1 className='text-center text-2xl md:text-3xl lg:text-4xl font-medium text-slate-700 mt-10'>React With TypeScript</h1>
       <Greeting userName={name} age={age} />
+
+      <div className='text-center'>
+        <h1 className='text-2xl text-slate-700'>Total count: <span className='text-red-600 font-medium'>{count}</span></h1>
+        <button className='outline-none border border-gray-200 rounded-md px-6 py-1 mt-2 hover:bg-gray-100 active:bg-transparent text-slate-700' onClick={() => setCount(count + 1)}>Increment</button>
+      </div>
+
+      {
+        user ? <p className='text-center text-2xl text-slate-700 pt-6'>Username: {user?.name}, Email: {user?.email}</p> : undefined
+      }
+      <div className='flex items-center justify-center mt-2'>
+        <button className='outline-none border border-gray-200 rounded-md px-6 py-1 mt-2 hover:bg-gray-100 active:bg-transparent text-slate-700' onClick={() => setUser({name: "Walidur Rahman", email: "walidur.rahman@gmail.com"})}>Set New User</button>
+      </div>
     </>
   )
 }
