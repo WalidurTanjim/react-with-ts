fd9b5e89de70a7d4658eccb3ea695fb1599a4125
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-07-15 02:04:02 +0600

    npm react-router-dom added for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,20 @@
-import { useEffect, useState } from 'react';
 import './App.css'
-import Greeting from './components/Greeting/Greeting'
-
-interface UserInterface {
-  name: string;
-  email: string;
-}
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import MainLayout from './layout/MainLayout';
+import Home from './pages/Home/Home';
+import SignIn from './pages/SignIn/SignIn';
 
 function App() {
-  const [count, setCount] = useState<number>(0);
-  const [user, setUser] = useState<UserInterface | null>(null);
+  const router = createBrowserRouter([
+    {
+      path: '/', element: <MainLayout />, children: [
+        { path: '/', element: <Home /> },
+        { path: '/signin', element: <SignIn /> }
+      ]
+    }
+  ])
 
-  const name = "Walidur Tanjim";
-  const age = 25;
-
-  useEffect((): void => {
-    setUser({name: "WALIDUR TANJIM", email: "walidur.tanjim@gmail.com"})
-  }, []);
-
-  return (
-    <>
-      <h1 className='text-center text-2xl md:text-3xl lg:text-4xl font-medium text-slate-700 mt-10'>React With TypeScript</h1>
-      <Greeting userName={name} age={age} />
-
-      <div className='text-center'>
-        <h1 className='text-2xl text-slate-700'>Total count: <span className='text-red-600 font-medium'>{count}</span></h1>
-        <button className='outline-none border border-gray-200 rounded-md px-6 py-1 mt-2 hover:bg-gray-100 active:bg-transparent text-slate-700' onClick={() => setCount(count + 1)}>Increment</button>
-      </div>
-
-      {
-        user ? <p className='text-center text-2xl text-slate-700 pt-6'>Username: {user?.name}, Email: {user?.email}</p> : undefined
-      }
-      <div className='flex items-center justify-center mt-2'>
-        <button className='outline-none border border-gray-200 rounded-md px-6 py-1 mt-2 hover:bg-gray-100 active:bg-transparent text-slate-700' onClick={() => setUser({name: "Walidur Rahman", email: "walidur.rahman@gmail.com"})}>Set New User</button>
-      </div>
-    </>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
---
+++ src/pages/SignIn/SignIn.tsx
@@ -0,0 +1,9 @@
+const SignIn = () => {
+    return (
+        <div className="container mx-auto w-full px-2 sm:px-6 lg:px-8 py-10">
+            <h1 className='text-center text-2xl md:text-3xl lg:text-4xl font-medium text-slate-700'>Sign In Form</h1>
+        </div>
+    );
+};
+
+export default SignIn;
