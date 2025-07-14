0c9ca813d804b766eeaea5b0b9ae64b087cee071
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-07-15 00:25:13 +0600

    This is my first project React with TypeScript. Hopefully Allah will help me to learn TypeScript properly. Project basic setup done

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-center text-4xl font-medium text-slate-700'>React With TypeScript</h1>
+    </>
+  )
+}
+
+export default App
